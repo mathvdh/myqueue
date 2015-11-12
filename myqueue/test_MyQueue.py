479adbee95d2ffa9c7238805e2c75923e0941b8b
Mathieu Van der Haegen <m@dcy.io>
2015-11-12 23:19:34 +0100

    Initial commit

---
+++ myqueue/test_MyQueue.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from MyQueue import MyQueue
+
+class TestMyQueue(unittest.TestCase):
+
+	def test_peek(self):
+		pass
+
+	def test_poll(self):
+		pass
+
+	def test_size(self):
+		pass
+
+	def test_add(self):
+		pass
